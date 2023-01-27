2f872fca4ef6ce3b784513c236b5487010a50d9e
Admin <Admin@TEB123008>
2023-01-27 09:08:20 +0100

    Add project files.

---
+++ Uppgift 2.4/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Uppgift_2_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Skriv in tre olika löner för tre olika anstälda på ett företag \nsen skrivs det ut medellönen \n");
+            Console.Write("1:a = ");
+            int x = Convert.ToInt32(Console.ReadLine());
+            Console.Write("2:a = ");
+            int y = Convert.ToInt32(Console.ReadLine());
+            Console.Write("3:e = ");
+            int z = Convert.ToInt32(Console.ReadLine());
+            int medel = (x + y + z) / 3;
+            Console.WriteLine("Medellönen är " + medel + " kr i månaden");
+        }
+    }
+}
